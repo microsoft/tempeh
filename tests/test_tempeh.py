2d251bdf02bee0aad045c07f48aebe4ca15b6ce0
Roman Lutz <rolutz@microsoft.com>
2019-11-08 14:27:35 -0500

    fix test parametrization
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- tests/test_tempeh.py
+++ tests/test_tempeh.py
@@ -11,11 +11,11 @@ from .conftest import get_selected_datasets, get_selected_models
 @pytest.mark.parametrize('Model', get_selected_models())
 @pytest.mark.parametrize("X_format", [np.ndarray, pd.DataFrame])
 @pytest.mark.parametrize("y_format", [np.ndarray, pd.Series])
-def test_fit_predict(Dataset, Model):
+def test_fit_predict(Dataset, Model, X_format, y_format):
     dataset = Dataset()
     model = Model()
     if model.compatible_with_dataset(dataset):
-        X_train, X_test = dataset.get_X(format=pd.DataFrame)
-        y_train, _ = dataset.get_y(format=pd.Series)
+        X_train, X_test = dataset.get_X(format=X_format)
+        y_train, _ = dataset.get_y(format=y_format)
         model.fit(X_train, y_train)
         model.predict(X_test)

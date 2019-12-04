75c1f658b08ea1dedb98e682bb490a4cb78e97fb
Roman Lutz <rolutz@microsoft.com>
2019-12-04 11:47:54 -0500

    add execution code from fairlearn tests
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

---
+++ tempeh/execution/azureml/environment_setup.py
@@ -0,0 +1,28 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+import shutil
+import subprocess
+import time
+
+from azureml.core import Environment
+
+
+
+def configure_environment(workspace, wheel_file=None):
+    # collect external requirements from requirements file 
+    environment = Environment.from_pip_requirements(name="env", file_path="requirements.txt")
+    
+    # add private pip wheel to blob if provided
+    if wheel_file:
+        private_pkg = environment.add_private_pip_wheel(workspace, file_path=wheel_file)
+        environment.python.conda_dependencies.add_pip_package(private_pkg)
+
+    # add azureml-sdk to log metrics
+    environment.python.conda_dependencies.add_pip_package("azureml-sdk")
+
+    # set docker to enabled for AmlCompute
+    environment.docker.enabled = True
+    print("environment successfully configured")
+    return environment

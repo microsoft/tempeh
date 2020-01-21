bdf13c03fa6c81486a6fb1c9a4e82388917ec6cf
Roman Lutz <rolutz@microsoft.com>
2020-01-21 13:58:37 -0500

    allow multiple sensitive features
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- tempeh/datasets/base_wrapper.py
+++ tempeh/datasets/base_wrapper.py
@@ -120,7 +120,7 @@ class BasePerformanceDatasetWrapper(object):
         else:
             raise ValueError("Only numpy.ndarray and pandas.Series are currently supported.")
 
-    def get_sensitive_features(self, name, format=np.ndarray):
+    def get_sensitive_features(self, names, format=np.ndarray):
         """ Returns the sensitive features of both the training and the test data. If the
         sensitive features don't exist under the specified name a ValueError is returned.
 
@@ -129,19 +129,53 @@ class BasePerformanceDatasetWrapper(object):
         :param format: either numpy.ndarray or pandas.Series
         :type format: type
         """
-        sensitive_features_train_name = "_{}_train".format(name)
-        sensitive_features_test_name = "_{}_test".format(name)
-        if not hasattr(self, sensitive_features_train_name) or \
-                not hasattr(self, sensitive_features_test_name):
-            raise ValueError("This dataset does not have sensitive features with the name {}."
-                             .format(name))
+        if type(names) == str:
+            # keeping the version with single string for backward compatibility
+            names = [names]
 
-        sensitive_features_train = getattr(self, sensitive_features_train_name).squeeze()
-        sensitive_features_test = getattr(self, sensitive_features_test_name).squeeze()
-        if format == np.ndarray:
-            return sensitive_features_train, sensitive_features_test
-        elif format == pd.Series:
+        if format not in [np.ndarray, pd.Series, pd.DataFrame]:
+            raise ValueError("Only numpy.ndarray, pandas.DataFrame and pandas.Series are "
+                             "currently supported.")
+
+        if len(names) > 1 and format not in [pd.DataFrame, np.ndarray]:
+            raise Exception("Multiple sensitive features were requested, so the only supported "
+                            "formats are pandas.DataFrame and numpy.ndarray.")
+
+        sensitive_features_train_names = {}
+        sensitive_features_test_names = {}
+        sensitive_features_train = {}
+        sensitive_features_test = {}
+        for name in names:
+            sensitive_features_train_names[name] = "_{}_train".format(name)
+            sensitive_features_test_names[name] = "_{}_test".format(name)
+            if not hasattr(self, sensitive_features_train_names[name]) or \
+                    not hasattr(self, sensitive_features_test_names[name]):
+                raise ValueError("This dataset does not have sensitive features with the name {}."
+                                 .format(name))
+
+            sensitive_features_train[name] = getattr(self, sensitive_features_train_names[name]).squeeze()  # noqa: E501
+            sensitive_features_test[name] = getattr(self, sensitive_features_test_names[name]).squeeze()  # noqa: E501
+
+        if format == pd.Series:
+            # we can assume that there's only one feature in this case
             return pd.Series(sensitive_features_train, name=name), \
                 pd.Series(sensitive_features_test, name=name)
-        else:
-            raise ValueError("Only numpy.ndarray and pandas.Series are currently supported.")
+
+        # for multiple features we need to merge the individual columns together before returning.
+        return _merge_columns_from_dict_into_matrix(sensitive_features_train,
+                                                    output_format=format), \
+            _merge_columns_from_dict_into_matrix(sensitive_features_test,
+                                                 output_format=format)
+
+
+def _merge_columns_from_dict_into_matrix(column_dict, output_format=np.ndarray):
+    array = np.concatenate(column_dict.values(), axis=1)
+
+    if output_format == np.ndarray:
+        return array
+
+    if output_format != pd.DataFrame:
+        raise Exception("Merging columns into a matrix is only supported with output_format "
+                        "numpy.ndarray and pandas.DataFrame.")
+
+    return pd.DataFrame(array, columns=column_dict.keys())
